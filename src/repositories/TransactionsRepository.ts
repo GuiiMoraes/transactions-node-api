81549aa6e4af5ab3f0139fb8434470cce5245bcf
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-05-03 14:05:08 -0300

    feat(*): create transitions repositories

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactions {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (accumulator: number, currentValue: number): number =>
+      accumulator + currentValue;
+
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const incomeValues = incomeTransactions.map(
+      incomeTransaction => incomeTransaction.value,
+    );
+    const incomesSum = incomeValues.reduce(reducer, 0);
+
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const outcomeValues = outcomeTransactions.map(
+      outcomeTransaction => outcomeTransaction.value,
+    );
+    const outcomesSum = outcomeValues.reduce(reducer, 0);
+
+    const totalSum = incomesSum - outcomesSum;
+
+    return {
+      income: incomesSum,
+      outcome: outcomesSum,
+      total: totalSum,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactions): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
 // import CreateTransactionService from '../services/CreateTransactionService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +26,21 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type }: Request = request.body;
+
+    if (type === 'outcome') {
+      const balance = transactionsRepository.getBalance();
+
+      if (balance.total - value < 0) {
+        return response
+          .status(400)
+          .json({ error: 'Outcome value is bigger than total' });
+      }
+    }
+
+    const transactions = transactionsRepository.create({ title, value, type });
+
+    return response.json(transactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
